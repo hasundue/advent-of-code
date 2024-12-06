140da124367ed8d9003bef7e57be77cb808a7868
Shun Ueda <hasundue@gmail.com>
2024-12-06 07:53:03 +0000

    refactor day6

--- 2024/6/main.ts
+++ 2024/6/main.ts
@@ -1,13 +1,15 @@
 type Pos = [number, number];
 type Direction = [number, number];
-type Map = string[][];
+type FloorMap = string[][];
 
-const parseMap = (input: string): Map =>
+const getMap = (input: string): FloorMap =>
   input.split("\n").slice(0, -1).map((line) => line.split(""));
 
-const getSize = (map: Map): [number, number] => [map.length, map[0].length];
+const getSize = (
+  map: FloorMap,
+): [number, number] => [map.length, map[0].length];
 
-const getStartPos = (map: Map): Pos =>
+const getStartPos = (map: FloorMap): Pos =>
   map.reduce<Pos | undefined>((pos, row, i) => {
     if (pos) return pos;
     const j = row.indexOf("^");
@@ -22,35 +24,57 @@ const move = (
 const isOutside = (pos: Pos, size: [number, number]): boolean =>
   pos[0] < 0 || pos[0] >= size[0] || pos[1] < 0 || pos[1] >= size[1];
 
-const examine = (map: Map, pos: Pos): string => map[pos[0]][pos[1]];
+const examine = (map: FloorMap, pos: Pos): string => map[pos[0]][pos[1]];
 
 const turn = (direction: Direction): Direction => [direction[1], -direction[0]];
 
-export function partOne(input: string): number {
-  const map = parseMap(input);
+const isSame = (a: Direction, b: Direction): boolean =>
+  a[0] === b[0] && a[1] === b[1];
 
-  const start = getStartPos(map);
+function patrol(
+  map: FloorMap,
+  start: Pos,
+  direction: Direction,
+) {
   const size = getSize(map);
 
-  const visited = new Map<number, Set<number>>();
+  const visited = new Map<number, Map<number, Direction>>();
   let curr = start;
-  let direction: Direction = [-1, 0];
+  let dir: Direction = direction;
 
   while (true) {
-    const set = visited.get(curr[0]) ?? new Set();
-    visited.set(curr[0], set.add(curr[1]));
+    const forward = move(curr, dir);
 
-    const next = move(curr, direction);
-    if (isOutside(next, size)) {
+    const inner = visited.get(curr[0]) ??
+      visited.set(curr[0], new Map()).get(curr[0])!;
+
+    const last = inner.get(curr[1]);
+
+    if (last && isSame(last, turn(dir))) {
+      //
+    }
+    inner.set(curr[1], dir);
+
+    if (isOutside(forward, size)) {
       break;
     }
-    if (examine(map, next) === "#") {
-      direction = turn(direction);
-      curr = move(curr, direction);
+    if (examine(map, forward) === "#") {
+      dir = turn(dir);
+      curr = move(curr, dir);
     } else {
-      curr = next;
+      curr = forward;
     }
   }
+  return visited;
+}
+
+export function partOne(input: string): number {
+  const map = getMap(input);
+
+  const start = getStartPos(map);
+  const visited = patrol(map, start, [-1, 0]);
+  console.log(visited);
+
   return visited.values().reduce((total, set) => total + set.size, 0);
 }
 
