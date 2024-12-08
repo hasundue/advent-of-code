e465b204f2bd86340eee1c2c31824f7b827aca5f
Shun Ueda <hasundue@gmail.com>
2024-12-08 08:57:06 +0000

    solve day8 part1

--- 2024/8/main.ts
+++ 2024/8/main.ts
@@ -1,39 +1,119 @@
-class SparseMatrix<T> {
-  constructor(private maps: Map<number, Map<number, T>> = new Map()) {}
+import { distinctBy } from "jsr:@std/collections";
 
-  get(row: number, col: number): T | undefined {
+type Antenna = {
+  row: number;
+  col: number;
+  freq: string;
+};
+
+function groupByFreq(antennas: Antenna[]): Antenna[][] {
+  const obj = Object.groupBy(
+    antennas,
+    (it) => it.freq,
+  ) as Record<string, Antenna[]>;
+  return Object.values(obj);
+}
+
+function combinations<T>(arr: T[]): [T, T][] {
+  return arr.flatMap((_, i) =>
+    arr.slice(i + 1).map((x) => [arr[i], x] as [T, T])
+  );
+}
+
+type Offset = {
+  row: number;
+  col: number;
+};
+
+function getOffset(a: Antenna, b: Antenna): Offset {
+  return {
+    row: b.row - a.row,
+    col: b.col - a.col,
+  };
+}
+
+type Antinode = {
+  row: number;
+  col: number;
+};
+
+class AntennaMap {
+  private maps: Map<number, Map<number, string>> = new Map();
+  readonly size: [number, number];
+
+  constructor(input: string) {
+    const lines = input.split("\n").slice(0, -1);
+    lines.forEach((line, row) => {
+      line.split("").forEach((char, col) => {
+        if (char !== ".") {
+          this.set(row, col, char);
+        }
+      });
+    });
+    this.size = [lines.length, lines[0].length];
+  }
+
+  get(row: number, col: number): string | undefined {
     return this.maps.get(row)?.get(col);
   }
-  set(row: number, col: number, value: T): SparseMatrix<T> {
+
+  set(row: number, col: number, freq: string): AntennaMap {
     if (!this.maps.has(row)) {
       this.maps.set(row, new Map());
     }
-    this.maps.get(row)!.set(col, value);
+    this.maps.get(row)!.set(col, freq);
     return this;
+  }
+
+  list(): Antenna[] {
+    const antennas: Antenna[] = [];
+    this.maps.forEach((cols, row) => {
+      cols.forEach((freq, col) => {
+        antennas.push({ row, col, freq });
+      });
+    });
+    return antennas;
+  }
+
+  covers(row: number, col: number): boolean {
+    return row >= 0 && row < this.size[0] && col >= 0 && col < this.size[1];
+  }
+
+  antinodes(): Antinode[] {
+    const entries = this.list();
+    const antinodes = groupByFreq(entries).flatMap(
+      (group) => this.antinodesForFreq(group),
+    );
+    return distinctBy(antinodes, (it) => `${it.row},${it.col}`);
+  }
+
+  private antinodesForFreq(
+    antennas: Antenna[],
+  ): Antinode[] {
+    const pairs = combinations(antennas);
+    return pairs.flatMap((pair) => this.antinodesForPair(pair));
+  }
+
+  private antinodesForPair([a, b]: [Antenna, Antenna]): Antinode[] {
+    const offset = getOffset(a, b);
+
+    const candidates: Antinode[] = [
+      { row: a.row - offset.row, col: a.col - offset.col },
+      { row: b.row + offset.row, col: b.col + offset.col },
+    ];
+    return candidates.filter((it) => this.covers(it.row, it.col));
   }
 }
 
-function parseInput(input: string): SparseMatrix<string> {
-  const antennas = new SparseMatrix<string>();
-  input.split("\n").slice(0, -1).forEach((line, row) => {
-    line.split("").forEach((char, col) => {
-      if (char !== ".") {
-        antennas.set(row, col, char);
-      }
-    });
-  });
-  return antennas;
-}
-
-export function partOne(input: string): number {
-  const antennas = parseInput(input);
-  console.log(antennas);
-  return 0;
+export function countAntinodes(input: string): number {
+  const antennas = new AntennaMap(input);
+  const antinodes = antennas.antinodes();
+  return antinodes.length;
 }
 
 if (import.meta.main) {
   const input = await Deno.readTextFile(
     new URL("./input.txt", import.meta.url),
   );
-  console.log(partOne(input));
+  console.log(countAntinodes(input));
 }
--- 2024/8/test.ts
+++ 2024/8/test.ts
@@ -1,10 +1,10 @@
 import { assertEquals } from "jsr:@std/assert";
-import { partOne } from "./main.ts";
+import { countAntinodes } from "./main.ts";
 
 const example = await Deno.readTextFile(
   new URL("./example.txt", import.meta.url),
 );
 
 Deno.test("partOne", () => {
-  assertEquals(partOne(example), 14);
+  assertEquals(countAntinodes(example), 14);
 });
