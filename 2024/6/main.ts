32ae2da68dc97fc3fb4335c7ed3727b254bb53a6
Shun Ueda <hasundue@gmail.com>
2024-12-08 23:14:07 +0000

    refactor day6

--- 2024/6/main.ts
+++ 2024/6/main.ts
@@ -1,106 +1,136 @@
-type Pos = [number, number];
+type Position = [number, number];
+
+const Position = {
+  move: (pos: Position, direction: Direction): Position => [
+    pos[0] + direction[0],
+    pos[1] + direction[1],
+  ],
+  toString: (pos: Position): string => `${pos[0]},${pos[1]}`,
+};
+
 type Direction = [number, number];
-type FloorMap = string[][];
 
-const getMap = (input: string): FloorMap =>
-  input.split("\n").slice(0, -1).map((line) => line.split(""));
+const Direction = {
+  match: (a: Direction, b: Direction): boolean =>
+    a[0] === b[0] && a[1] === b[1],
+  turn: (direction: Direction): Direction => [direction[1], -direction[0]],
+  toString: (direction: Direction): string => `${direction[0]},${direction[1]}`,
+};
 
-const getSize = (
-  map: FloorMap,
-): [number, number] => [map.length, map[0].length];
+class FloorMap {
+  readonly size: [number, number];
 
-const getStartPos = (map: FloorMap): Pos =>
-  map.reduce<Pos | undefined>((pos, row, i) => {
-    if (pos) return pos;
-    const j = row.indexOf("^");
-    return j !== -1 ? [i, j] : undefined;
-  }, undefined) as Pos;
+  constructor(public rows: string[][]) {
+    this.size = [rows.length, rows[0].length];
+  }
 
-const move = (
-  pos: Pos,
-  direction: Direction,
-): Pos => [pos[0] + direction[0], pos[1] + direction[1]];
+  static fromInput(input: string): FloorMap {
+    return new FloorMap(
+      input.split("\n").slice(0, -1).map((line) => line.split("")),
+    );
+  }
 
-const isOutside = (pos: Pos, size: [number, number]): boolean =>
-  pos[0] < 0 || pos[0] >= size[0] || pos[1] < 0 || pos[1] >= size[1];
+  replace(
+    row: number,
+    col: number,
+    replacement: string,
+  ): FloorMap {
+    return new FloorMap(
+      this.rows.map((r, i) =>
+        i === row ? r.map((c, j) => (j === col ? replacement : c)) : r
+      ),
+    );
+  }
 
-const examine = (map: FloorMap, pos: Pos): string => map[pos[0]][pos[1]];
+  get start(): Position {
+    return this.rows.reduce<Position | undefined>((pos, row, i) => {
+      if (pos) return pos;
+      const j = row.indexOf("^");
+      return j !== -1 ? [i, j] : undefined;
+    }, undefined) as Position;
+  }
 
-const turn = (direction: Direction): Direction => [direction[1], -direction[0]];
+  covers(pos: Position): boolean {
+    return pos[0] >= 0 && pos[0] < this.size[0] && pos[1] >= 0 &&
+      pos[1] < this.size[1];
+  }
 
-const isSame = (a: Direction, b: Direction): boolean =>
-  a[0] === b[0] && a[1] === b[1];
+  examine(pos: Position): string | undefined {
+    return this.covers(pos) ? this.rows[pos[0]][pos[1]] : undefined;
+  }
+}
 
-function patrol(
-  map: FloorMap,
-  start: Pos,
-  direction: Direction,
-  fn: (curr: Pos, direction: Direction) => void = () => {},
-) {
-  const size = getSize(map);
+class InfiniteLoopError extends Error {
+  constructor(public position: Position) {
+    super();
+  }
+}
 
-  const visited = new Map<number, Map<number, Direction>>();
-  let curr = start;
-  let dir: Direction = direction;
+function patrol(map: FloorMap, options?: {
+  start?: Position;
+  direction?: Direction;
+  visited?: Map<string, string>;
+  check?: (
+    curr: Position,
+    direction: Direction,
+    visited: Map<string, string>,
+  ) => void;
+}) {
+  const visited = options?.visited ?? new Map();
+
+  let curr = options?.start ?? map.start;
+  let dir = options?.direction ?? [-1, 0];
 
   while (true) {
-    fn(curr, dir);
+    const last = visited.get(Position.toString(curr));
 
-    const forward = move(curr, dir);
-
-    const inner = visited.get(curr[0]) ??
-      visited.set(curr[0], new Map()).get(curr[0])!;
-
-    const last = inner.get(curr[1]);
-
-    if (last && isSame(last, dir)) {
-      throw start; // infinite loop
+    if (last && last === Direction.toString(dir)) {
+      throw new InfiniteLoopError(curr);
     }
-    inner.set(curr[1], dir);
 
-    if (isOutside(forward, size)) {
+    options?.check?.(curr, dir, new Map(visited));
+
+    const front = map.examine(Position.move(curr, dir));
+
+    if (front === "#") {
+      dir = Direction.turn(dir);
+    }
+    visited.set(Position.toString(curr), Direction.toString(dir));
+
+    if (!front) {
       break;
     }
-    if (examine(map, forward) === "#") {
-      dir = turn(dir);
-      curr = move(curr, dir);
-    } else {
-      curr = forward;
-    }
+    curr = Position.move(curr, dir);
   }
   return visited;
 }
 
 export function partOne(input: string): number {
-  const map = getMap(input);
-  const start = getStartPos(map);
-  const visited = patrol(map, start, [-1, 0]);
-
-  return visited.values().reduce((total, set) => total + set.size, 0);
+  const map = FloorMap.fromInput(input);
+  const visited = patrol(map);
+  return visited.size;
 }
 
 export function partTwo(input: string): number {
-  const map = getMap(input);
-  const start = getStartPos(map);
-  const found = new Map<number, Set<number>>();
-  patrol(map, start, [-1, 0], (curr: Pos, dir: Direction) => {
-    const front = move(curr, dir);
-    try {
-      patrol(
-        map.map((row, i) =>
-          i === front[0] ? row.toSpliced(front[1], 1, "#") : row
-        ),
-        curr,
-        dir,
-      );
-    } catch {
-      const front = move(curr, dir);
-      const set = found.get(front[0]) ??
-        found.set(front[0], new Set()).get(front[0])!;
-      set.add(front[1]);
-    }
+  const map = FloorMap.fromInput(input);
+  const found = new Set<string>();
+  patrol(map, {
+    check: (curr, direction, visited) => {
+      const front = Position.move(curr, direction);
+      try {
+        patrol(map.replace(front[0], front[1], "#"), {
+          start: curr,
+          direction,
+          visited,
+        });
+      } catch (e) {
+        if (e instanceof InfiniteLoopError) {
+          found.add(`${front[0]},${front[1]}`);
+        }
+      }
+    },
   });
-  return found.values().reduce((total, set) => total + set.size, 0);
+  return found.size;
 }
 
 if (import.meta.main) {
