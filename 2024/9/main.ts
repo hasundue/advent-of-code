d9633ebae187a6bc2400783dcf9f8a941a0b1a08
Shun Ueda <hasundue@gmail.com>
2024-12-10 08:45:22 +0000

    solve day9

--- 2024/9/main.ts
+++ 2024/9/main.ts
@@ -1,17 +1,40 @@
-type Disk = (number | null)[];
+type Disk = (File | Space)[];
+
+type File = {
+  id: number;
+  size: number;
+};
+
+type Space = {
+  id: undefined;
+  size: number;
+};
+
+function isFile(it: File | Space): it is File {
+  return it.id !== undefined;
+}
 
 function decode(input: string): Disk {
   const map = input.slice(0, -1).split("").map(Number);
-  const disk: Disk = [];
-  map.forEach((num, i) => {
-    for (let k = 0; k < num; k++) {
-      disk.push(i % 2 === 0 ? i / 2 : null);
-    }
-  });
-  return disk;
+  return map.map((num, i) =>
+    i % 2 === 0 ? { id: i / 2, size: num } : { size: num }
+  );
 }
 
-function compact(disk: Disk): Disk {
+type Blocks = (number | null)[];
+
+function serialize(disk: Disk): Blocks {
+  const blocks: Blocks = [];
+  disk.forEach((it) => {
+    const block = isFile(it) ? it.id : null;
+    for (let k = 0; k < it.size; k++) {
+      blocks.push(block);
+    }
+  });
+  return blocks;
+}
+
+function compactBlocks(disk: Blocks): Blocks {
   disk.forEach((value, i) => {
     if (value !== null) {
       return;
@@ -20,24 +43,57 @@ function compact(disk: Disk): Disk {
     if (j <= i) {
       return;
     }
-    const moving = disk[j];
-    disk[i] = moving;
+    disk[i] = disk[j];
     disk[j] = null;
   });
   return disk;
 }
 
-export function checksum(input: string) {
+function checksum(compacted: Blocks) {
+  return compacted
+    .map((it) => it ? it : 0)
+    .reduce((total, curr, i) => total + curr * i);
+}
+
+export function partOne(input: string) {
+  const disk = decode(input);
+  const blocks = serialize(disk);
+  const compacted = compactBlocks(blocks);
+  return checksum(compacted);
+}
+
+function compact(disk: Disk): Disk {
+  let len = disk.length;
+  for (let i = 0; i < len; i++) {
+    const curr = disk[i];
+    if (isFile(curr)) {
+      continue;
+    }
+    let j = disk.findLastIndex((it) => isFile(it) && it.size <= curr.size);
+    if (j <= i) {
+      continue;
+    }
+    disk[i] = { ...disk[j] };
+    const rem = curr.size - disk[j].size;
+    if (rem > 0) {
+      disk.splice(i + 1, 0, { id: undefined, size: rem });
+      j++;
+      len++;
+    }
+    disk[j] = { id: undefined, size: disk[j].size };
+  }
+  return disk;
+}
+
+export function partTwo(input: string) {
   const disk = decode(input);
   const compacted = compact(disk);
-  const data = compacted.slice(
-    0,
-    compacted.findIndex((it) => it === null),
-  ) as number[];
-  return data.reduce((total, curr, i) => total + curr * i);
+  const blocks = serialize(compacted);
+  return checksum(blocks);
 }
 
 if (import.meta.main) {
   const input = await Deno.readTextFile("2024/9/input.txt");
-  console.log(checksum(input));
+  console.log(partOne(input));
+  console.log(partTwo(input));
 }
--- 2024/9/test.ts
+++ 2024/9/test.ts
@@ -1,10 +1,14 @@
 import { assertEquals } from "jsr:@std/assert";
-import { checksum } from "./main.ts";
+import { partOne, partTwo } from "./main.ts";
 
 const example = await Deno.readTextFile(
   new URL("./example.txt", import.meta.url),
 );
 
 Deno.test("Day9 - Part One", () => {
-  assertEquals(checksum(example), 1928);
+  assertEquals(partOne(example), 1928);
+});
+
+Deno.test("Day9 - Part Two", () => {
+  assertEquals(partTwo(example), 2858);
 });
